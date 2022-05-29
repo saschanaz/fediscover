56dc2db0699b0a02bb22a81856ff7a9110ee3da9
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-05-29 18:47:47 +0200

    Add more things to rendered status

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import html from "https://cdn.jsdelivr.net/npm/nanohtml@1/+esm";
 /** @type {Promise<import("./third_party/masto.js").MastoClient>} */
 export let mastoReady;
 
+/**
+ * @param {string} domain
+ * @param {*} post
+ */
+function computeLocalWebUrl(domain, post) {
+  return new URL(`web/@${post.account.acct}/${post.id}`, domain).toString();
+}
+
 async function main() {
   mastoReady = maybeAuthorizeViaForm(document.body);
 
@@ -13,9 +21,16 @@ async function main() {
   const rediscover = new Rediscover(masto);
   for await (const post of rediscover.fetchRecentRandomPosts()) {
     document.body.append(html`
-      <article>
+      <article lang=${post.language || ""}>
         <h1>${post.account.displayName}</h1>
         ${document.createRange().createContextualFragment(post.content)}
+        <div>
+          <a href=${computeLocalWebUrl(masto.config.url, post)} target="_blank"
+            ><time datetime=${post.createdAt}
+              >${new Date(post.createdAt)}</time
+            ></a
+          >
+        </div>
       </article>
     `);
   }
